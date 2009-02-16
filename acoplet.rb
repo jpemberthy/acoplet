4f280b84b04e688c14a22fb185c7a04c7c545eb6
jpemberthy <jpemberthy@gmail.com>
2009-02-16 16:40:49 -0500

    Adding contact

--- acoplet.rb
+++ acoplet.rb
@@ -1,6 +1,23 @@
 require "rubygems"
 require "sinatra"
 
+helpers do
+  
+  def base_url
+    host = "http://#{Sinatra::Application.host}"
+    port = Sinatra::Application.port == 80 ? host : host << ":#{Sinatra.Application.port}"
+  end
+  
+  def url(path='')
+    [base_url, path].join('/')
+  end
+  
+end
+
 get '/' do
   erb :home
+end
+
+get '/contact' do
+  erb :contact
 end
